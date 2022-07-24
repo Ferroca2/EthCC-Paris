76d6e8c7e8150c0c279ed05124b901d50f642f93
André Thiessen <almthiessen@gmail.com>
2022-07-24 01:36:58 -0300

    refactor: update create page

--- src/components/create-allocation/index.js
+++ src/components/create-allocation/index.js
@@ -1,8 +1,10 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Select } from "antd";
 import FormItem from "antd/lib/form/FormItem";
 import React from "react";
 import { Link } from 'react-router-dom'
 import styles from './create.module.css';
+
+const { Option } = Select;
 
 export default function CreateAllocation () {
     return(
@@ -21,19 +23,33 @@ export default function CreateAllocation () {
                     className={styles.formsContainerSmaller}
                 >
                     <div className={styles.buySellContainer} >
-                        <FormItem label={<label style={{ color: "white" }}>Sell</label>} labelCol={{span: 24, offset: 10}}>
-                            <Input className={styles.formsInput} />
+                        <FormItem label={<label style={{ color: "white", fontSize: "20px" }}>Sell</label>} labelCol={{span: 24, offset: 9}}>
+                            <Select defaultValue="USDC" style={{ width: "120px" }} className={styles.formsSelect}>
+                                <Option value="USDC">USDC</Option>
+                                <Option value="BRL">BRL</Option>
+                                <Option value="EUR">EUR</Option>
+                            </Select>
+                            {/* <Input className={styles.formsInput} /> */}
                         </FormItem>
-                        <FormItem label={<label style={{ color: "white", textAlign: "center" }}>Buy</label>} labelCol={{span: 24, offset: 10}}>
-                            <Input className={styles.formsInput} />
+                        <img src="troca.png" alt="img" width="35px" height="35px"/>
+                        <FormItem label={<label style={{ color: "white", fontSize: "20px" }}>Buy</label>} labelCol={{span: 24, offset: 10}}>
+                        <Select defaultValue="USDC" style={{ width: "120px" }} className={styles.formsSelect}>
+                                <Option value="USDC">USDC</Option>
+                                <Option value="BRL">BRL</Option>
+                                <Option value="EUR">EUR</Option>
+                        </Select>
                         </FormItem>
                     </div>
-                    <FormItem label={<label style={{ color: "white", marginTop: "40px" }}>Amount</label>} labelCol={{span: 24, offset: 10}} >
+                    <FormItem label={<label style={{ color: "white", marginTop: "30px", fontSize: "20px" }}>Amount</label>} labelCol={{span: 24, offset: 10}} >
                         <Input className={styles.formsInput} />
                     </FormItem>
-                    <FormItem label={<label style={{ color: "white", marginTop: "25px" }}>Period</label>} labelCol={{span: 24, offset: 10}}>
+                    <FormItem label={<label style={{ color: "white", marginTop: "5px", fontSize: "20px" }}>Period</label>} labelCol={{span: 24, offset: 10}}>
                         <Input className={styles.formsInput} />
                     </FormItem>
+                    <FormItem label={<label style={{ color: "white", marginTop: "5px", fontSize: "20px" }}>Email</label>} labelCol={{span: 24, offset: 10}}>
+                        <Input className={styles.formsInput} />
+                    </FormItem>
+                    <Button className={styles.button1}>Criar Posição</Button>
                 </Form>
             </div>
         </>
