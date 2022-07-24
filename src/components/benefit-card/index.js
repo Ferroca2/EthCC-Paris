619e560b069d02fb049617d2a3c50003fe86f1ef
Kalil Georges Balech <kalilgb152@gmail.com>
2022-07-24 05:33:26 -0300

    home change

--- src/components/benefit-card/index.js
+++ src/components/benefit-card/index.js
@@ -6,7 +6,7 @@ export default function BenefitCard (props) {
     return(
         <div className={styles.allCard}>
             <div className={styles.circle}>
-                
+                <img src={props.url}></img>
             </div>
             <div className={styles.benefitDescription}>{props.sentence}</div>
         </div>
